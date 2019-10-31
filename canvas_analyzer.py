fc94fbcff7a4c7b4960816dfcaeb2da240df380c
cuprehan <51897056+cuprehan@users.noreply.github.com>
2019-10-31 09:26:21 -0400

    functions 1-7 finished

--- canvas_analyzer.py
+++ canvas_analyzer.py
@@ -11,18 +11,79 @@ To test your current solution, run the `test_my_solution.py` file.
 Refer to the instructions on Canvas for more information.
 
 "I have neither given nor received help on this assignment."
-author: YOUR NAME HERE
+author: Rehan Amir
 """
 __version__ = 7
 
 # 1) main
+import canvas_requests
+def main(name):
+    user = canvas_requests.get_user(name)
+    print_user_info(user)
+    courses = canvas_requests.get_courses(name)
+    courses = filter_available_courses(courses)
+    print_courses(courses)
+    course_ids = get_course_ids(courses)
+    course_id = choose_course(course_ids)
+    submissions = canvas_requests.get_submissions(name,course_id)
+    summarize_points(submissions)
+    summarize_groups(submissions)
+    plot_scores(submissions)
+    plot_grade_trends(submissions)
 # 2) print_user_info
+def print_user_info(user):
+    print("Name: " + user["name"])
+    print("Title: " + user["title"])
+    print("Primary Email: " + user["primary_email"])
+    print("Bio: " + user["bio"])
+
+bio = {
+    "name": "Hermione Granger",
+    "title": "Student",
+    "primary Email": "hgranger@hogwarts.edu",
+    "bio": "Interested in Magic, Learning, and House Elf Rights"
+    }
 # 3) filter_available_courses
+def filter_available_courses(courses):
+    available_courses = []
+    for course in courses:
+        if course["workflow_state"] == "available":
+            available_courses.append(course)
+    return available_courses
+classes = {'workflow_state': 'available', 'end_at': '2017-12-24T17:06:00Z', 'id': 52, 'start_at': '2017-08-21T17:06:00Z', 'name': 'Introduction to Programming in Python'}, {'workflow_state': 'available', 'end_at': '2017-12-24T17:06:00Z', 'id': 15, 'start_at': '2017-08-21T17:06:00Z', 'name': 'Potions'}, {'workflow_state': 'available', 'end_at': '2017-12-24T17:06:00Z', 'id': 23, 'start_at': '2017-08-21T17:06:00Z', 'name': 'Defense Against the Dark Arts'}, {'workflow_state': 'available', 'end_at': '2017-12-24T17:06:00Z', 'id': 34, 'start_at': '2017-08-21T17:06:00Z', 'name': 'Transfiguration'}
+
 # 4) print_courses
+def print_courses(courses):
+    for course in courses:
+        print(course["id"] + " : " + "name")
 # 5) get_course_ids
+def get_course_ids(courses):
+    course_ids = []
+    for course in courses:
+        course_ids.append(course["id"])
+    return course_ids
 # 6) choose_course
+def choose_course(course_ids):
+    chosen_course = input("Pick a course id:")
+    chosen_course = int(chosen_course)
+    while chosen_course not in course_ids:
+        chosen_course = int(input("Pick a course id:"))
+    return chosen_course
 # 7) summarize_points
+def summarize_points(submissions):
+    points_possible = 0
+    points_obtained = 0
+
+    for submission for submissions:
+        points_possible = points_possible + (submission["assignment"]["points_possible"] * submission["assignment"]["points_possible"]["group"]["group_weight"])
+        points_obtained = points_obtained + (submission["score"] * submission["assignment"]["points_possible"]["group"]["group_weight"])
+    print("Points possible so far: " + str(points_possible))
+    print("Points obtained: " + str(points_obtained))
+    print("Current grade: " + str(round((points_obtained/points_possible) * 100), 1))
 # 8) summarize_groups
+def summarize_groups(submissions):
+    unweighted_grade = sum(submissions)
+
 # 9) plot_scores
 # 10) plot_grade_trends
 
